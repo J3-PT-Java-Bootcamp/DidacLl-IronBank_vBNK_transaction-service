8592f881730cdec86f870a7677c5b8ba00459f52
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-05 22:44:12 +0200

    configure connexions

--- src/main/java/com/ironhack/vbnk_transactionservice/VBnkTransactionServiceApplication.java
+++ src/main/java/com/ironhack/vbnk_transactionservice/VBnkTransactionServiceApplication.java
@@ -2,8 +2,11 @@ package com.ironhack.vbnk_transactionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VBnkTransactionServiceApplication {
 
 	public static void main(String[] args) {
