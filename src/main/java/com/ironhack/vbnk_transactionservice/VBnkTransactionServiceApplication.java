47531f764b60de3f66b3b7a9dbbf233af987bcc0
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-05 02:46:48 +0200

    BASIC_ARCH: all services created, and develop structure under vBNK_sys

---
+++ src/main/java/com/ironhack/vbnk_transactionservice/VBnkTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.vbnk_transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VBnkTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VBnkTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/vbnk_transactionservice/VBnkTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.vbnk_transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VBnkTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
