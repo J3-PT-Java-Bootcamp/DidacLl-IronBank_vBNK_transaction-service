1022d1d75e61b80ba0bc4341e7c750b13b564077
VladScv <ddcllrnsbrv@uoc.edu>
2022-09-09 15:19:40 +0200

    TransactionService implementation

--- src/main/java/com/ironhack/vbnk_transactionservice/controllers/TransactionController.java
+++ src/main/java/com/ironhack/vbnk_transactionservice/controllers/TransactionController.java
@@ -1,4 +1,18 @@
 package com.ironhack.vbnk_transactionservice.controllers;
 
+import com.ironhack.vbnk_transactionservice.data.TransactionStatus;
+import com.ironhack.vbnk_transactionservice.data.dao.VBTransaction;
+import com.ironhack.vbnk_transactionservice.data.dto.TransactionDTO;
+
+import java.net.http.HttpResponse;
+import java.time.Instant;
+import java.util.List;
+
 public interface TransactionController {
+    HttpResponse<TransactionDTO> getTransaction(String id);
+    HttpResponse<TransactionDTO> updateTransaction(boolean isOk);
+    void deleteTransaction(String id);
+    HttpResponse<List<TransactionDTO>> getAllTransByDate(Instant date);
+
+
 }
---
+++ src/main/java/com/ironhack/vbnk_transactionservice/controllers/impl/TransactionControllerWeb.java
@@ -0,0 +1,30 @@
+package com.ironhack.vbnk_transactionservice.controllers.impl;
+
+import com.ironhack.vbnk_transactionservice.controllers.TransactionController;
+import com.ironhack.vbnk_transactionservice.data.dto.TransactionDTO;
+
+import java.net.http.HttpResponse;
+import java.time.Instant;
+import java.util.List;
+
+public class TransactionControllerWeb implements TransactionController {
+    @Override
+    public HttpResponse<TransactionDTO> getTransaction(String id) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse<TransactionDTO> updateTransaction(boolean isOk) {
+        return null;
+    }
+
+    @Override
+    public void deleteTransaction(String id) {
+
+    }
+
+    @Override
+    public HttpResponse<List<TransactionDTO>> getAllTransByDate(Instant date) {
+        return null;
+    }
+}
